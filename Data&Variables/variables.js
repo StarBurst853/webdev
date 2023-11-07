387bf5c14dda24b56accdcafb71d48718a759c64
Scholar <scholar@paradigm.local>
2023-11-07 14:42:42 -0700

    variables

---
+++ Data&Variables/variables.js
@@ -0,0 +1,40 @@
+let firstname = "Ryan"
+let favFood = "Pizza";
+let leastFavFood = "onions";
+
+let age = 14;
+let time = 60;
+let weight = 125;
+
+let Silly = true;
+let Serious = false;
+let Goofy = true;
+
+const ingrediants = {FirstIngrediant:"Pepporoni", SecondIngrediant:"Cheese"};
+const Games = {Firstgame:"Minecraft", SecondGame:"Rocket League"}
+const BoardGames = {FirstBoardGame:"Chess", SecondBoardGame:"Monopoly"};
+
+const BurgerIngrediants = ["cheese", "lettuce", "tomato"];
+const Meat = ["chicken", "beef", "bacon"];
+const Friends = ["connor", "Kellan", "landon"];
+
+document.getElementById("1").innerHTML = firstname;
+document.getElementById("2").innerHTML = favFood;
+document.getElementById("3").innerHTML = leastFavFood;
+
+document.getElementById("4").innerHTML = age;
+document.getElementById("5").innerHTML = time;
+document.getElementById("6").innerHTML = weight;
+
+document.getElementById("7").innerHTML = Silly;
+document.getElementById("8").innerHTML = Serious;
+document.getElementById("9").innerHTML = Goofy;
+
+document.getElementById("10").innerHTML = ingrediants;
+document.getElementById("11").innerHTML = Games;
+document.getElementById("12").innerHTML = BoardGames;
+
+document.getElementById("13").innerHTML = BurgerIngrediants;
+document.getElementById("14").innerHTML = Meat;
+document.getElementById("15").innerHTML = Friends;
+
