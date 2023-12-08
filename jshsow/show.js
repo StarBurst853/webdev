8bb412a418bc86de4ec4f895b9306281b0072aaf
Scholar <scholar@paradigm.local>
2023-12-08 12:36:31 -0700

    skills showcase

---
+++ jshsow/show.js
@@ -0,0 +1,19 @@
+document.getElementById("info1").addEventListener("click", function(){
+    document.getElementById("info1").innerHTML = "This is a picture of a goose standing in a swampy area.";
+});
+document.getElementById("info2").addEventListener("click", function(){
+    document.getElementById("info2").innerHTML = "This is a picture of a goose with blue eyes looking at the cameraman.";
+});
+function calc(){
+    let income = document.getElementById("income").value;
+    let price = document.getElementById("price").value;
+    let food = document.getElementById("food").value;
+    let cage = document.getElementById("cage").value;
+    let baby = document.getElementById("baby").value;
+    let health = document.getElementById("health").value;
+    let other = document.getElementById("other").value;
+
+    let total = Number(income) + Number(price) + Number(food) + Number(cage) + Number(baby) + Number(health) + Number(other);
+
+    document.getElementById("total").innerHTML = `total: ${total}`;
+}
